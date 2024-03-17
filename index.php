68d0ba392a40b5949441f962fa0bc2ecd6a79082
rupom <mohrajulrupom@gmail.com>
2024-03-17 12:19:59 +0600

    oop style

--- index.php
+++ index.php
@@ -6,5 +6,15 @@
  * Description: asset manage for a plugin
  * 
  */
-
+define('AST_DIR',plugin_dir_url( __FILE__ )."/assets");
+ class assetmanage{
+    function __construct(){
+        add_action('wp_enqueue_scripts',array($this,'enqueue_assets'));
+    }
+    function enqueue_assets(){
+        wp_enqueue_script( 'ast_main_js', AST_DIR.'/js/main.js',array('jquery','ast_another_js'),time(),true);
+        wp_enqueue_script( 'ast_another_js',AST_DIR.'/js/another.js',array('jquery'),time(),true);
+    }
+ }
+ new assetmanage();
 ?>
